6ad36da13e0b4e97e6d89d86b0ace8dace8c3207
KawaCassie <cassandraguillet@gmail.com>
2021-08-05 17:10:32 +0200

    Ajouts logos, icones, header

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import Vuetify from 'vuetify';
+import store from './store/index';
+import { sync } from 'vuex-router-sync';
 
-createApp(App).use(store).mount('#app')
+Vue.config.productionTip = false;
+
+const moment = require ('moment')
+require('moment/locale/fr')
+Vue.use(require('vue-moment'), {
+    moment
+});
+Vue.use(Vuetify);
+const unsync = sync(store, router);
+
+new Vue({
+    router, 
+    store, 
+    Vuetify, 
+    render: (h) => h(App),
+}).$mount('#app');
+
+unsync();
