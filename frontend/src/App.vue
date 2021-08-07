4da6a918998f4edb6d2285504f41e535e5c88d8f
KawaCassie <cassandraguillet@gmail.com>
2021-08-07 09:27:53 +0200

    Ajout images et fichier scss

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import '../public/style.scss';
 
 export default {
   name: 'App',
