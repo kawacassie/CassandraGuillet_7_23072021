92cda3fd96335b06b1d9825b8a02f9e30e75fee3
KawaCassie <cassandraguillet@gmail.com>
2021-08-15 20:12:21 +0200

    Ajout page Posts + corrections controllers

---
+++ frontend/src/components/NoPost.vue
@@ -0,0 +1,13 @@
+<template>
+    <main>
+        <h1>Bienvenue sur le réseau social interne de Groupomania</h1>
+        <h2>Il n'y a pas encore de post pour le moment...</h2>
+        <h2>Soyez le premier à écrire quelque chose !</h2>
+    </main>
+</template>
+
+<script>
+export default {
+    Name: "NoPost"
+}
+</script>
