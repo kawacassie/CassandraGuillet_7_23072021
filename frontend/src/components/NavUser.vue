82eb670e32241fbfdba8b6191c20fd5c478861c2
KawaCassie <cassandraguillet@gmail.com>
2021-09-07 12:44:29 +0200

    Ajout img nav

--- frontend/src/components/NavUser.vue
+++ frontend/src/components/NavUser.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="img_name">
-            <img v-if="avatar ==! null" :src="avatar" alt="Image de profil" height="40"> 
+            <img v-if="avatar" :src="avatar" alt="Image de profil" height="40"> 
             <img v-else src="../assets/default-avatar.png" alt="Avatar par défaut" height="40">
             <span id="user_name">{{ first_name + " " + last_name }}</span>
         </div>
@@ -21,6 +21,7 @@
 
 <script>
 import Swal from 'sweetalert2'
+import axios from 'axios'
 
 export default {
     name: "NavUser",
@@ -28,13 +29,32 @@ export default {
         return {
             first_name: "",
             last_name: "",
-            avatar: null
+            avatar: ""
         }
     },
     created: function() {
-        this.first_name = localStorage.getItem("first_name")
-        this.last_name = localStorage.getItem("last_name")
-        this.avatar = localStorage.getItem("avatar")
+        axios.get('http://localhost:3000/api/users/accounts/' + localStorage.getItem("userId"), { headers : { "Authorization" : localStorage.getItem("token")} })
+        .then(user => {
+            this.first_name = user.data.first_name
+            this.last_name = user.data.last_name
+            this.avatar = user.data.avatar
+        })
+        .catch(function(error) {
+            const codeError = error.message.split("code ")[1]
+            let messageError = ""
+            switch (codeError){
+                case "400" : messageError = "Vos informations n'ont pas été récupérées"; break
+                case "401" : messageError = "Requête non-authentifiée"; break
+            }
+            Swal.fire({
+                title: "Une erreur est survenue",
+                text: messageError || error.message,
+                icon: "error",
+                timer: 1500,
+                showConfirmButton: false,
+                timerProgressBar: true
+            })
+        })
     },
     methods: {
         masquerDiv(id) {
